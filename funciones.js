97ab4930dc2ddc1d4281d3d7a2f381274be5c06b
nalberaDH <105679268+nalberaDH@users.noreply.github.com>
2023-07-26 20:53:18 -0300

    agrego funcionalidad para despedir y sumar

--- funciones.js
+++ funciones.js
@@ -2,5 +2,12 @@ const saludar = (nombre) => {
     console.log(`Hola ${nombre}`);
 }
 
+const despedir = (nombre) => {
+    return null;
+}
+
+const getSuma = () => {
+    return 'Hola soy suma'
+}
 
 module.exports = saludar;
